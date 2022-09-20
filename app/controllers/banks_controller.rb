6a65590df078d230d72858368ccd16360cbdb587
Angela Zaldua <angelazaldua29@gmail.com>
2022-09-20 17:04:55 -0500

    spec bank method index

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,2 +1,7 @@
 class BanksController < ApplicationController
+  def index
+    @banks = Bank.all
+
+    render json: @banks, status: :ok
+  end
 end
--- spec/requests/banks_spec.rb
+++ spec/requests/banks_spec.rb
@@ -1,7 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Banks", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Banks', type: :request do
+  describe 'GET /index' do
+    context 'when exist data' do
+      let!(:bank) { create(:bank) }
+
+      it 'should return a bank' do
+        get '/banks'
+        payload = JSON.parse(response.body)
+        expect(payload).not_to be_empty
+        expect(payload[0]['id']).to eq(bank.id)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when not exist data' do
+      it 'should return nil' do
+        get '/banks'
+        payload = JSON.parse(response.body)
+        expect(payload).to be_empty
+        expect(response).to have_http_status(:ok)
+      end
+    end
   end
 end
