9d63888fa0fb7cf51bb204e15c1e2cc7241f9384
Angela Zaldua <angelazaldua29@gmail.com>
2022-09-20 19:13:29 -0500

    spec bank method create

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -4,4 +4,20 @@ class BanksController < ApplicationController
 
     render json: @banks, status: :ok
   end
+
+  def create
+    @bank = Bank.new(val_params)
+
+    if @bank.save
+      render json: @bank, status: :ok
+    else
+      render json: { error: "Couldn't create bank'" }, status: :bad_request
+    end
+  end
+
+  private
+
+  def val_params
+    params.require(:bank).permit(:name)
+  end
 end
--- spec/requests/banks_spec.rb
+++ spec/requests/banks_spec.rb
@@ -15,11 +15,54 @@ RSpec.describe 'Banks', type: :request do
     end
 
     context 'when not exist data' do
-      it 'should return nil' do
+      it 'should return empty data' do
         get '/banks'
         payload = JSON.parse(response.body)
         expect(payload).to be_empty
         expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  describe 'POST /create' do
+    context 'when data is ok' do
+      req_bank = { bank: { name: Faker::Bank.name } }
+
+      before { post '/banks', params: req_bank }
+
+      it 'should return ok and create record' do
+        payload = JSON.parse(response.body)
+        expect(payload).not_to be_empty
+        expect(response).to have_http_status(:ok)
+      end
+    end
+
+    context 'when data is not ok' do
+      context 'when the name value is nil' do
+        req_bank = { bank: { name: nil } }
+
+        before { post '/banks', params: req_bank }
+
+        it 'should return bad_request' do
+          payload = JSON.parse(response.body)
+          expect(payload).not_to be_empty
+          expect(payload['error']).to eq("Couldn't create bank'")
+          expect(response).to have_http_status(:bad_request)
+        end
+      end
+
+      context 'when the name value exceeds 50 characters' do
+        req_bank = { bank: { name: 'A' * 51 } }
+
+        before { post '/banks', params: req_bank }
+
+        it 'should return bad_request' do
+          payload = JSON.parse(response.body)
+          expect(payload).not_to be_empty
+          expect(req_bank[:bank][:name].size).not_to eq(50)
+          expect(payload['error']).to eq("Couldn't create bank'")
+          expect(response).to have_http_status(:bad_request)
+        end
       end
     end
   end
